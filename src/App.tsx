74908b8d11115d44f0386401ab7bf5e10b6a6f77
Gaurav Agrawal <agrawalgaurav866@gmail.com>
2024-12-07 21:08:29 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import "./App.css";
-import AdvertisingBanner from "./components/Reusecomponents/Advertising/AdvertisingBanner";
-import BlogPageContent from "./components/Reusecomponents/Blogs/Blogs";
-import Footer from "./components/Reusecomponents/Footer/Footer";
-import GlobalInfoPart from "./components/Reusecomponents/GlobalBlock/GlobalBlock";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Heroscreen from "./screen/Hero2.0/Hero2.0";
 import Header from "./components/Reusecomponents/Header/Header";
+import Footer from "./components/Reusecomponents/Footer/Footer";
+import ContactUs from "./screen/ContactUs/ContactUs";
 import Partner_page from "./screen/Partner-page/Partner_page";
-import HubnexUser from "./components/Reusecomponents/Hubnex/Hubnex";
-import Benefits from "./components/Reusecomponents/Benefits/Benefits";
+import Blog_page from "./screen/Blog-page/Blog_page";
+import CaseStudy from "./screen/casestudy/CaseStudyScreen";
 
-const App = () => {
-  return (
-    <>
-      <Header />
-
-      <Partner_page />
-
-      <HubnexUser />
-
-      <Benefits />
-
-      <BlogPageContent />
-
-      <GlobalInfoPart />
-
-      <AdvertisingBanner />
-
-      <Footer />
-    </>
-  );
-};
+const App: React.FC = () => (
+  <>
+    <Header />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
+        <Route path="/home" element={<Heroscreen />}></Route>
+        <Route path="/contact-us" element={<ContactUs></ContactUs>}></Route>
+        <Route path="/partners" element={<Partner_page></Partner_page>}></Route>
+        <Route path="/blogs" element={<Blog_page></Blog_page>}></Route>
+        <Route path="/case-study" element={<CaseStudy></CaseStudy>}></Route>
+      </Routes>
+    </BrowserRouter>
+    <Footer />
+  </>
+);
 
 export default App;
